6c355299d646c6523422ab00d93c703a790a6f4a
Muhammad Zaky <devmuzaky@gmail.com>
2022-09-28 19:07:28 +0200

    fix bug in game logic

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ btnHold.addEventListener('click', function () {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
             // Finish the game
             playing = false;
             diceEl.classList.add('hidden');
